cd6caae889facce46b320df5d523949d646850ee
Georgi <gardax95@gmail.com>
2014-03-02 07:14:22 +0200

    add detailed user model

---
+++ LybrarySystemServices/LibrarySystem.Services/Models/DetailedUserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibrarySystem.Services.Models
+{
+    public class DetailedUserModel
+    {
+        public int Id { get; set; }
+
+        public int UniqueNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<BookToReturnModel> BooksToReturn { get; set; }
+    }
+}
